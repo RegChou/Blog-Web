69b08d8489cccb8ca4ae45e3e8cb7ecf58dbda9a
ouyangzongheng <ouyangzoheng@163.com>
2020-06-15 16:45:39 +0800

    搭建项目

--- src/main/java/com/muxui/blog/BlogApplication.java
+++ src/main/java/com/muxui/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.muxui.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //开启定时任务
 public class BlogApplication {
 
     public static void main(String[] args) {
